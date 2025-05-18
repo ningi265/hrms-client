026ff8405ebd47aefb0321be6b6382f4f9400068
ningi265 <ningintonga@gmail.com>
2025-05-18 14:22:56 +0200

    UI

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -570,7 +570,7 @@ const user = authUser ? {
               display: "grid", 
               gap: 3, 
               gridTemplateColumns: { 
-                xs:  "13fr 7fr",
+                xs:  "1fr 1fr",
               },
               mb: 4,
               minWidth: "600px"
